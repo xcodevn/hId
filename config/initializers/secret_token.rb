ccd9ba01415c43263bbea4fef8f5cc18a6188cee
Thong <xcodevn@gmail.com>
2014-01-25 22:10:35 +0700

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HId::Application.config.secret_key_base = 'fbc6bfd30d0a61d35a9a72e82551879e8aa3330f025323decbca51e57a3f8cebc768a540b0ca40204eeb34fe1ae00ec2b8c3d10a0ca13cba7408da0f390b3624'
