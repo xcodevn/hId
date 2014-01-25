20bb2be912b41e5261a35501f66b397773c33d6a
Thong <xcodevn@gmail.com>
2014-01-25 23:23:54 +0700

    Add 3 new resources: accounts, answers, questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 HId::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+	resources :accounts
+	resources :questions
+	resources :answers
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
